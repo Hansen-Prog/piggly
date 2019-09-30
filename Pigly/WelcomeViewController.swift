cbb034db8c5efe7f80607015bd9da62fee83b010
Marcus Jensen <marcusjensen@brokenmac.local>
2019-09-30 13:15:06 +0200

    git commit -m "new VC class"

---
+++ Pigly/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  Pigly
+//
+//  Created by Marcus Jensen on 30/09/2019.
+//  Copyright © 2019 Andreas Hansen. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
